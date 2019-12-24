eb3b89e45feacdcec8d342421e597c746fd3b8be
yuce <yuce@jd.com>
2019-12-24 14:55:39 +0800

    update cropper

--- js/demo.js
+++ js/demo.js
@@ -1,7 +1,7 @@
 $(function () {
   $.get('../tpl/demo.tpl',function(data){
     var html = template.compile(data)
-    $('body').html(html)
+    $('body').append(html)
 
     layui.use('carousel', function() {
       var carousel = layui.carousel
@@ -13,4 +13,31 @@ $(function () {
       })
     })
   })
+
+  layui.config({
+    base: '../libs/layui/cropper/' //layui自定义layui组件目录
+  }).use(['form','croppers'], function () {
+    var $ = layui.jquery
+      ,form = layui.form
+      ,croppers = layui.croppers
+      ,layer= layui.layer;
+
+    croppers.render({
+      elem: '.editimg'
+      ,saveW: 120
+      ,saveH: 120
+      ,mark: 1/1
+      ,area: '900px'
+      ,url: "/upload"
+      ,done: function (res, ele) {
+        if (res.success) {
+          layer.msg('upload success',{icon: 1});
+          layer.closeAll('page');
+          console.log($(ele))
+        } else {
+          layer.alert('upload failed',{icon: 2});
+        }
+      }
+    });
+  });
 })
